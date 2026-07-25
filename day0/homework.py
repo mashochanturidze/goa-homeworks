7e04bc5d5e479f349959637942de48a02b7091c4
mashochanturidze <mashochanturidze57@gmail.com>
2026-07-25 18:20:59 +0400

    second commit

---
+++ day0/homework.py
@@ -0,0 +1,19 @@
+#დავალება პირველი
+name="berdia"
+print(name)
+#შეცდომა იყო იმიტომ, რომ berdia ბრჭყალებში არ ეწერა
+
+#დავალება მეორე
+#snake_case არის როცა სიტყვებს შორის _ იწერება
+#მაგალითად:
+my_name="masho"
+print(my_name)
+
+#მესამე დავალება
+my_name="sandro" 
+print(my_name)
+
+#მეოთხე დავალება
+first_number=2
+second_number=10
+print(first_number * second_number)
