f9cc9ef29138522d231d9c5f541022099dc51824
mashochanturidze <mashochanturidze57@gmail.com>
2026-08-14 15:28:29 +0400

    second commit

---
+++ day6/Classwork/classwork.py
@@ -0,0 +1,40 @@
+#N1
+name = input("Enter your name: ")
+print("Hello " + name)
+
+#N2
+name = "Masho"
+age = 13
+height = 1.65
+
+print(name)
+print(age)
+print(height)
+
+#N3
+user_name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello " + user_name + " " + last_name)
+
+#N4
+# Input არის ინფორმაცია, რომელსაც მომხმარებელი პროგრამაში შეიყვანს.
+# Output არის ინფორმაცია, რომელსაც პროგრამა ეკრანზე გამოიტანს.
+# კარგი პრაქტიკაა კოდის სუფთად დაწერა,
+# გასაგები ცვლადების სახელების გამოყენება და კომენტარების დამატება.
+
+#N5
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+#N6
+# int არის მთელი რიცხვი, მაგალითად 10
+# float არის ათწილადი რიცხვი, მაგალითად 10.5
+# str არის ტექსტი, მაგალითად "Hello"
+
+
---
+++ day6/Homework/homework.py
@@ -0,0 +1,44 @@
+#1
+a=5
+b="10"
+print(a+int(b))
+
+#2
+a=5
+b="10"
+print(str(a)+b)
+
+#3
+num = int(input("enter your number:"))
+num2 = 5
+
+print(num + num2)
+
+#4
+a="5"
+print(int(a))
+
+
+#5
+a=6
+print(float(a))
+
+#6
+a=14
+b=7
+print(a/b)
+# / ჩვეულებრივი გაყოფაა და პასუხი არის ათწილადი
+
+a=14
+b=7
+print(a//b)
+# //  მთელი გაყოფაა და პასუხი არის მთელი რიცხვი
+
+#7
+age=int(input("enter your age"))
+print(age + 5)
+
+#8
+name=input("enter your name")
+last_name=input("enter your last name")
+print("hello"+ name + last_name)
